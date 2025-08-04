744f8b10a7abdb27585b42959a428291e5627587
aljimsondev <aljimson.megrino@gmail.com>
2025-08-04 20:48:26 +0800

    Chore | added nest/config

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    // Load ConfigModule which assists with environment variables
+    ConfigModule.forRoot({ envFilePath: '.env' }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
