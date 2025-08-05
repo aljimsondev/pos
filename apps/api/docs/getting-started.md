da32809b733b18c932a5a45ba7ffb10fa3cb6db0
aljimsondev <aljimson.megrino@gmail.com>
2025-08-05 14:06:16 +0800

    Chore | added swagger components and configuration

---
+++ apps/api/docs/getting-started.md
@@ -0,0 +1,47 @@
+# Getting Started
+
+Welcome to the S-Tix API documentation.
+
+## Generating access token
+
+All requests must contain authentication token.
+
+#### API Key Header [Internal oath use only]
+
+Complete the following steps to make your first API call via API Key Header:
+
+- Get Access Token from frontend Authentication. `Note: if you are currently logged-in to frontend authentication. it will automatically attached the access token to the header per request`
+- You may set the API key manually at the Authentication tab and mark the `api_key` option.
+- Utilize any resource you want to access or query
+
+#### Cookie Token [Internal oath use only]
+
+Complete the following steps to make your first API call via Cookie Token:
+
+- Get Access Token from frontend Authentication. `Note: if you are currently logged-in to frontend authentication. it will automatically attached the cookie token to the header per request`
+- Utilize any resource you want to access or query
+
+---
+
+## Resources
+
+| Resource                                               | Description                |
+| ------------------------------------------------------ | -------------------------- |
+| [➡️ Organization Resource](/api/docs#tag/organization) | Manage organizations       |
+| [➡️ Event Resource](/api/docs#tag/events)              | Manage events              |
+| [➡️ Payment Resource](/api/docs#tag/payments)          | Payment transactions       |
+| [➡️ Customer Resource](/api/docs#tag/customer)         | Customers/Public endpoints |
+| [➡️ Analytics Resource](/api/docs#tag/analytics)       | Analytics queries          |
+
+---
+
+## Errors
+
+| Errors                                     | Code  | Type             | Description                                                     |
+| ------------------------------------------ | ----- | ---------------- | --------------------------------------------------------------- |
+| User not found [CODE: AD001]               | AD001 | Permission Error | User ID or Email not found                                      |
+| Insufficient permissions [CODE: AD002]     | AD002 | Permission Error | Insufficient permissions. User not found or expired role access |
+| Permission denied [CODE: AD003]            | AD003 | Permission Error | Permission not match with access role                           |
+| Event not found [CODE: AD004]              | AD004 | Permission Error | Event not found from found event ID                             |
+| Invalid event organization [CODE: AD005]   | AD005 | Permission Error | Organization not found associated with event ID                 |
+| Invalid default organization [CODE: AD006] | AD006 | Permission Error | Error generating default personal organization ID               |
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,20 +1,140 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { Version } from 'src/version';
+import {
+  DocumentBuilder,
+  OpenAPIObject,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 import { AppModule } from './app.module';
+
+const API_PREFIX = 'api';
+
+// Helper function to filter Swagger document paths by tag
+function filterDocumentByTag(
+  document: any,
+  tagToInclude?: string | null,
+): OpenAPIObject {
+  const clonedDocument = JSON.parse(JSON.stringify(document));
+
+  if (!tagToInclude) {
+    return clonedDocument;
+  }
+  for (const path in clonedDocument.paths) {
+    for (const method in clonedDocument.paths[path]) {
+      const operation = clonedDocument.paths[path][method];
+
+      // If operation doesn't have the tag we want, remove it
+
+      if (!operation.tags?.includes(tagToInclude)) {
+        delete clonedDocument.paths[path][method];
+      }
+    }
+
+    // If path has no methods left, remove the path
+    if (Object.keys(clonedDocument.paths[path]).length === 0) {
+      delete clonedDocument.paths[path];
+    }
+  }
+
+  return clonedDocument;
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const markdownDescription = readFileSync(
+    join('__dirname', '../docs', 'getting-started.md'),
+    'utf8',
+  );
+
+  //OpenAPI Documentation
   const config = new DocumentBuilder()
-    .setTitle('POS API')
-    .setDescription('The POS API endpoints')
-    .setVersion(Version[1])
-    .addTag('POS')
+    .setTitle('S-Tix Documentation')
+    .addApiKey(
+      {
+        type: 'apiKey',
+        in: 'header',
+        name: 'X-Access-Token',
+      },
+      'Access Token [Internal Use]',
+    )
+    .addCookieAuth(
+      'AuthToken.id',
+      {
+        type: 'apiKey',
+      },
+      'Auth Cookie [Internal Use]',
+    )
+    .addApiKey(
+      {
+        type: 'apiKey',
+        in: 'header',
+        name: 'X-API-Key',
+      },
+      'API Key [External Integration]',
+    )
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'JWT Bearer Token [External Integration]',
+    )
+    .setDescription(markdownDescription)
+    .setVersion('0.0.1')
+    .addTag('Getting Started')
+    .addTag('Organization', 'Manage Event Organization resources endpoints')
+    .addTag('Events', 'Manage Event ticketing resources endpoints')
+    .addTag('Bookings', 'Manage booking events')
+    .addTag('Analytics', 'Get analytics data')
+    .addTag('Customer', 'Customer endpoints')
+    .addTag('Features', 'Public features list queries')
     .build();
 
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  const documentFactory = () => SwaggerModule.createDocument(app, config, {});
 
-  await app.listen(process.env.PORT ?? 3000);
+  //create filtered document
+  const externalDocument = filterDocumentByTag(documentFactory(), 'external');
+  const internalDocument = filterDocumentByTag(documentFactory(), null);
+
+  const externalOptions: SwaggerCustomOptions = {
+    explorer: true,
+
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+      withCredentials: true,
+      persisAuthorization: true, // persist authentication across refreshes
+    },
+    customSiteTitle: 'API Documentation [Integration]',
+    customCss: `.swagger-ui .topbar { background-color: #0078d7; }`,
+  }; // external integration
+
+  SwaggerModule.setup('ui/i', app, () => externalDocument, externalOptions); // external integration
+
+  //Scalar Reference
+  app.use(
+    '/docs',
+    apiReference({
+      spec: { content: internalDocument },
+      hideModels: true,
+      hideDownloadButton: true,
+      metaData: { title: 'S-Tix API [Internal]' },
+    }),
+  );
+
+  SwaggerModule.setup('api', app, documentFactory);
+  app.setGlobalPrefix(API_PREFIX);
+
+  const PORT = process.env.PORT ?? 3000;
+
+  await app.listen(PORT);
+  Logger.debug('Server starts in port: ' + PORT);
+  Logger.debug(`You can view it in here: http://localhost:${PORT}/api`);
 }
 bootstrap();
