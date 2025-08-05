ae192a9722bfa326e93d784f8a52c94c21e42e24
aljimsondev <aljimson.megrino@gmail.com>
2025-08-05 13:45:08 +0800

    Chore | added version

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Version } from 'src/version';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('POS API')
+    .setDescription('The POS API endpoints')
+    .setVersion(Version[1])
+    .addTag('POS')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
---
+++ apps/api/src/version.ts
@@ -0,0 +1,4 @@
+export const Version = {
+  1: '1', //initial iteration
+  2: '2',
+} as const;
