10eff997d9ee92de8f5e71e1966b7dab7c316bf9
aljimsondev <aljimson.megrino@gmail.com>
2025-08-05 14:23:57 +0800

    Chore | added environment variable declaration

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -13,8 +13,8 @@ import { UserModule } from './user/user.module';
     ConfigModule.forRoot({ envFilePath: '.env' }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
