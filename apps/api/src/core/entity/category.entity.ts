8aa35d6b9e1d1cc0ef4d619785e165ecd893b0cf
aljimsondev <aljimson.megrino@gmail.com>
2025-08-05 16:25:28 +0800

    Chore | updated entity property

--- apps/api/src/core/entity/category.entity.ts
+++ apps/api/src/core/entity/category.entity.ts
@@ -1,10 +1,14 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Product } from 'src/core/entity/product.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity({ name: 'categories' })
-export class Product {
+export class Category {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
   category_name: string;
+
+  @OneToMany(() => Product, (product) => product)
+  product: Product;
 }
--- apps/api/src/core/entity/product.entity.ts
+++ apps/api/src/core/entity/product.entity.ts
@@ -1,4 +1,5 @@
 import { Brand } from 'src/core/entity/brand.entity';
+import { Category } from 'src/core/entity/category.entity';
 import { Photo } from 'src/core/entity/photo.entity';
 import {
   Column,
@@ -22,4 +23,22 @@ export class Product {
   // product can only have one brand
   @ManyToOne(() => Brand, (brand) => brand.product)
   brand: Brand;
+
+  @ManyToOne(() => Category, (category) => category.product)
+  category: Category;
+
+  @Column()
+  description: string;
+
+  @Column()
+  barcode: string;
+
+  @Column()
+  created_at: Date;
+
+  @Column()
+  updated_at: Date;
+
+  @Column('jsonb', { default: {} })
+  metadata: Record<string, any>;
 }
