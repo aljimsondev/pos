4e53de1c3a3e0c0acbb3599ca9450c4370c8c64d
aljimsondev <aljimson.megrino@gmail.com>
2025-08-09 19:21:50 +0800

    Chore | remove typeorm module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,20 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_PIPE } from '@nestjs/core';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '@thallesp/nestjs-better-auth';
 import { ZodValidationPipe } from 'nestjs-zod';
 import { auth } from 'src/core/auth/auth';
-import { Brand } from 'src/core/entity/brand.entity';
-import { Category } from 'src/core/entity/category.entity';
-import { LineItem } from 'src/core/entity/line-items.entity';
-import { Photo } from 'src/core/entity/photo.entity';
-import { Product } from 'src/core/entity/product.entity';
-import { Sale } from 'src/core/entity/sale.entity';
-import { Variation } from 'src/core/entity/variation.entity';
 import { DatabaseModule } from 'src/core/modules/db';
 import { RedisModule, RedisService } from 'src/core/modules/redis';
-import { User } from './core/entity/user.entity';
 import { ProductModule } from './resources/product/product.module';
 import { UserModule } from './resources/user/user.module';
 
@@ -30,25 +21,6 @@ import { UserModule } from './resources/user/user.module';
       },
     }),
     AuthModule.forRoot(auth),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT!,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: [
-        User,
-        Product,
-        LineItem,
-        Photo,
-        Brand,
-        Sale,
-        Variation,
-        Category,
-      ],
-      synchronize: true, // disable for production
-    }),
     UserModule,
     ProductModule,
   ],
