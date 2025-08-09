a8a921ef293ceb0e7d453a4c87b3d531ab46ded2
aljimsondev <aljimson.megrino@gmail.com>
2025-08-09 17:41:23 +0800

    Chore | updated scalar

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,7 @@ import {
   VersioningType,
 } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { apiReference } from '@scalar/nestjs-api-reference';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
@@ -19,36 +19,6 @@ const API_PREFIX = 'api';
 const VERSION_PREFIX = 'v';
 
 const PORT = process.env.PORT ?? 4001;
-
-// Helper function to filter Swagger document paths by tag
-function filterDocumentByTag(
-  document: any,
-  tagToInclude?: string | null,
-): OpenAPIObject {
-  const clonedDocument = JSON.parse(JSON.stringify(document));
-
-  if (!tagToInclude) {
-    return clonedDocument;
-  }
-  for (const path in clonedDocument.paths) {
-    for (const method in clonedDocument.paths[path]) {
-      const operation = clonedDocument.paths[path][method];
-
-      // If operation doesn't have the tag we want, remove it
-
-      if (!operation.tags?.includes(tagToInclude)) {
-        delete clonedDocument.paths[path][method];
-      }
-    }
-
-    // If path has no methods left, remove the path
-    if (Object.keys(clonedDocument.paths[path]).length === 0) {
-      delete clonedDocument.paths[path];
-    }
-  }
-
-  return clonedDocument;
-}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -94,64 +64,15 @@ async function bootstrap() {
   //OpenAPI Documentation
   const config = new DocumentBuilder()
     .setTitle('POS API Documentation')
-    .addApiKey(
-      {
-        type: 'apiKey',
-        in: 'header',
-        name: 'X-Access-Token',
-      },
-      'Access Token [Internal Use]',
-    )
-    .addCookieAuth(
-      'AuthToken.id',
-      {
-        type: 'apiKey',
-      },
-      'Auth Cookie [Internal Use]',
-    )
-    .addApiKey(
-      {
-        type: 'apiKey',
-        in: 'header',
-        name: 'X-API-Key',
-      },
-      'API Key [External Integration]',
-    )
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-      },
-      'JWT Bearer Token [External Integration]',
-    )
     .setDescription(markdownDescription)
-    .setVersion('0.0.1')
     .addTag('Getting Started')
-    .addTag('Organization', 'Manage Event Organization resources endpoints')
-    .addTag('Events', 'Manage Event ticketing resources endpoints')
-    .addTag('Bookings', 'Manage booking events')
-    .addTag('Analytics', 'Get analytics data')
-    .addTag('Customer', 'Customer endpoints')
-    .addTag('Features', 'Public features list queries')
     .build();
 
-  const documentFactory = () => SwaggerModule.createDocument(app, config, {});
-
-  const internalDocument = filterDocumentByTag(documentFactory(), null);
+  const document = SwaggerModule.createDocument(app, config, {});
 
   //Scalar Reference
-  app.use(
-    '/docs',
-    apiReference({
-      spec: { content: internalDocument },
-      hideModels: true,
-      hideDownloadButton: true,
-      metaData: { title: 'POS API [Internal]' },
-    }),
-  );
+  app.use('/docs', apiReference({ content: document }));
 
-  SwaggerModule.setup('api', app, documentFactory);
   app.setGlobalPrefix(API_PREFIX);
 
   await app.listen(PORT);
