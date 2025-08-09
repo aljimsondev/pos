827c3831cc5c75e050e5a142d10ea1a1f88a5ad0
aljimsondev <aljimson.megrino@gmail.com>
2025-08-09 18:54:22 +0800

    Chore | created betterAuth instance

--- apps/api/src/core/auth/auth.ts
+++ apps/api/src/core/auth/auth.ts
@@ -2,5 +2,5 @@ import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 
 export const auth = betterAuth({
-  database: drizzleAdapter(),
+  database: drizzleAdapter({}, { provider: 'pg' }),
 });
